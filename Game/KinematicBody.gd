c18e0afb710d57c46cc9ebcc9006542e81d47bce
David Uziel Guevara Hernández <daviduziel.gh@gmail.com>
2020-03-16 13:45:17 -0600

    Polished the demo

--- Game/KinematicBody.gd
+++ Game/KinematicBody.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 var gravity = -9.8
 var velocity = Vector3(0, 0, 0)
 
-const MAX_SPEED = 6
+const MAX_SPEED = 1
 const ACCELERATION = 1
 const DE_ACCELERATION = 5
 
@@ -11,16 +11,18 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
-	if(Input.is_action_just_pressed("move_fw")):
-		velocity.z -= ACCELERATION
-	if(Input.is_action_just_pressed("move_bw")):
-		velocity.z += ACCELERATION
+	if velocity.z < MAX_SPEED and velocity.z > -MAX_SPEED:
+		if(Input.is_action_just_pressed("move_fw")):
+			velocity.z -= ACCELERATION
+		if(Input.is_action_just_pressed("move_bw")):
+			velocity.z += ACCELERATION
 	if(Input.is_action_just_pressed("move_left")):
 		velocity.x += ACCELERATION
 	if(Input.is_action_just_pressed("move_right")):
 		velocity.x -= ACCELERATION
 	velocity.y += delta * gravity
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	velocity = move_and_slide(velocity, Vector3(0,-1,0))
+
 
 func move_in_direction():
 	pass
