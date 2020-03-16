5934613cd1c6754c22cab75d07c3eb19bec67d93
David Uziel Guevara Hernández <daviduziel.gh@gmail.com>
2020-03-15 20:01:08 -0600

    Finished enviroment test

---
+++ Game/KinematicBody.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+var gravity = -9.8
+var velocity = Vector3(0, 0, 0)
+
+const MAX_SPEED = 6
+const ACCELERATION = 1
+const DE_ACCELERATION = 5
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	if(Input.is_action_just_pressed("move_fw")):
+		velocity.z -= ACCELERATION
+	if(Input.is_action_just_pressed("move_bw")):
+		velocity.z += ACCELERATION
+	if(Input.is_action_just_pressed("move_left")):
+		velocity.x += ACCELERATION
+	if(Input.is_action_just_pressed("move_right")):
+		velocity.x -= ACCELERATION
+	velocity.y += delta * gravity
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+
+func move_in_direction():
+	pass
