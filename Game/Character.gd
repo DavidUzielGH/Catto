cccbeeda0ea50b76da65cc9849eb8384bc278945
David <daviduziel.gh@gmail.com>
2020-03-19 20:07:44 -0600

    Creados templates de personajes
    Brah

--- Game/Character.gd
+++ Game/Character.gd
@@ -1,10 +1,15 @@
 extends Node
 
 var char_name
-var scene 
+var scene_level
+var room
 
 func _ready():
+	room = get_parent()
 	global.get_scene(char_name)
 
 func set_name(nombre):
 	char_name = nombre
+
+func set_scene_level(nombre):
+	global.get_character_scene_level(char_name)
--- Game/Scripts/global.gd
+++ Game/Scripts/global.gd
@@ -1,12 +1,17 @@
 extends Node
 
-signal event
+onready var char_level = {"Maria":0, "Lugi":0, "Niño":0, "Escritor":0, "Maquinista":0}
+onready var room_level = {"Vagon1":0, "Techo1":0, "CosaNegra":0, "Techo3":0, "Vagon3":0, "Grua":0, "TechoGrua":0, "Techo4":0, "Caldera":0}
 
-var char_scene = {"mam":0, "tren":0, "niño":0, "escritor":0, "maquinista":0}
 
-func _ready():
-	
-	pass
+func raise_scene_level(character):
+	char_level[character]+=1
 
-func get_character_scene(character):
-	pass
+func get_character_scene_level(character):
+	return char_level[character]
+
+func get_room_level(room):
+	return room_level[room]
+
+func raise_room_level(room):
+	room_level[room]+=1
